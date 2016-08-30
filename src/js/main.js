0fa028a29d036001c8228bcbfe6427b5c6d6dd0b
yoichi kobayashi <info@tplh.net>
2016-08-30 17:54:02 +0900

    [add] first demo.

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,18 @@
+const Vue = require('vue');
+
 import initCommon from './init/common.js'
 import initIndex from './init/index.js'
 
 const { pathname } = window.location;
 
 const init = () => {
+  new Vue({
+    el: '#app',
+    data: {
+      message: 'Hello Vue.js!'
+    }
+  });
+
   initCommon();
   switch (pathname.replace('index.html', '')) {
     case '/':
