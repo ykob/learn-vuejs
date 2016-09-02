a040477e44cd5126dc0375c6cd3d7d113f0768c4
yoichi kobayashi <info@tplh.net>
2016-09-02 20:48:22 +0900

    [add] two classes to start page.

--- src/js/init/start.js
+++ src/js/init/start.js
@@ -2,9 +2,32 @@ const Vue = require('vue');
 
 export default function() {
   new Vue({
-    el: '#app',
+    el: '#app-twoway',
     data: {
       message: 'Hello Vue.js!'
     }
   });
+
+  new Vue({
+    el: '#app-renderlist',
+    data: {
+      todos: [
+        { text: 'Learn JavaScript' },
+        { text: 'Learn Vue.js' },
+        { text: 'Build Something Awesome' }
+      ]
+    }
+  });
+
+  new Vue({
+    el: '#app-hundle-user-input',
+    data: {
+      message: 'Hello Vue.js'
+    },
+    methods: {
+      reverseMessage: function() {
+        this.message = this.message.split('').reverse().join('')
+      }
+    }
+  })
 };
