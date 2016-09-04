1a034776f680038b0274cfd4900972985696a3f5
yoichi kobayashi <info@tplh.net>
2016-09-05 00:13:51 +0900

    [add] example setter function to computed page.

--- src/js/init/computed.js
+++ src/js/init/computed.js
@@ -1,7 +1,7 @@
 const Vue = require('vue');
 
 export default function() {
-  const vue_basic = new Vue({
+  const vm_basic = new Vue({
     el: '#app-basic',
     data: {
       a: 1
@@ -12,4 +12,30 @@ export default function() {
       }
     }
   });
+  console.log(vm_basic.b);
+  vm_basic.a = 2;
+  console.log(vm_basic.b);
+
+  const vm_setter = new Vue({
+    el: '#app-setter',
+    data: {
+      firstName: 'yoichi',
+      lastName: 'kobayashi'
+    },
+    computed: {
+      fullName: {
+        get: function() {
+          return this.firstName + ' ' + this.lastName
+        },
+        set: function(newValue) {
+          var names = newValue.split(' ');
+          this.firstName = names[0];
+          this.lastName = names[names.length - 1];
+        }
+      }
+    }
+  });
+  console.log(vm_setter.fullName);
+  vm_setter.fullName = 'taro yamada'
+  console.log(vm_setter.fullName);
 };
