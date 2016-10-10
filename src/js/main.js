6cd24d8b00d5fbbdc03e1e7b648c1b224db21930
yoichi kobayashi <info@tplh.net>
2016-10-10 23:47:13 +0900

    [add] created new page about 'template syntax' section.

--- src/js/main.js
+++ src/js/main.js
@@ -2,6 +2,7 @@ import initCommon from './init/common.js'
 import initIndex from './init/index.js'
 import initStart from './init/start.js'
 import initComputed from './init/computed.js'
+import initTemplate from './init/template.js'
 
 const { pathname } = window.location;
 
@@ -11,6 +12,7 @@ const init = () => {
     case '/': initIndex(); break;
     case '/start.html': initStart(); break;
     case '/computed.html': initComputed(); break;
+    case '/template.html': initTemplate(); break;
     default:
   }
 }
