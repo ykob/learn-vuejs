81d4f80a8c61c72d9629f98801c9ab02c4e738e9
yoichi kobayashi <info@tplh.net>
2016-10-12 16:11:58 +0900

    [add] created a new Vue model that is using filter functions.

--- src/js/init/template.js
+++ src/js/init/template.js
@@ -1,5 +1,25 @@
 const Vue = require('vue');
 
 export default function() {
-  
+  const vm1 = new Vue({
+    el: '#app',
+    data: {
+      number: 1,
+      ok: true
+    }
+  });
+
+  const vm2 = new Vue({
+    el: '#app-filters',
+    data: {
+      message: 'message'
+    },
+    filters: {
+      capitalize: function(value, arg1) {
+        if (!value) return ''
+        value = value.toString()
+        return value.charAt(0).toUpperCase() + value.slice(1) + ' ' + arg1
+      }
+    }
+  })
 }
