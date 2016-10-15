dd3ca94459fb831628357aae9c7c630d478e853b
yoichi kobayashi <info@tplh.net>
2016-10-15 23:35:56 +0900

    [add] create new page that is about how to binding class and style.

--- src/js/main.js
+++ src/js/main.js
@@ -2,7 +2,7 @@ import initCommon from './init/common.js'
 import initIndex from './init/index.js'
 import initStart from './init/start.js'
 import initComputed from './init/computed.js'
-import initTemplate from './init/template.js'
+import initStyle from './init/style.js'
 
 const { pathname } = window.location;
 
@@ -13,6 +13,7 @@ const init = () => {
     case '/start.html': initStart(); break;
     case '/computed.html': initComputed(); break;
     case '/template.html': initTemplate(); break;
+    case '/style.html': initTemplate(); break;
     default:
   }
 }
