84572b5f973c3febe24c68ed323bee6cca9d3ff8
yoichi kobayashi <info@tplh.net>
2016-10-26 18:51:55 +0900

    [add] write a new code that is using computed and methods function in v-for property.

--- src/js/init/list.js
+++ src/js/init/list.js
@@ -47,4 +47,24 @@ export default function() {
     }
   })
 
+  const vm5 = new Vue({
+    el: '#app-filter',
+    data: {
+      numbers: [ 1, 2, 3, 4, 5 ]
+    },
+    computed: {
+      evenNumbers: function() {
+        return this.numbers.filter(function(number) {
+          return number % 2 === 0;
+        })
+      }
+    },
+    methods: {
+      even: function(numbers) {
+        return numbers.filter(function(number) {
+          return number % 2 === 1;
+        })
+      }
+    }
+  })
 };
