dec5684c66c323d5e5da729fd9a0cddd3d64930d
yoichi kobayashi <info@tplh.net>
2016-10-28 19:06:25 +0900

    [add] a v-on property called Vue Object methods.

--- src/js/init/event.js
+++ src/js/init/event.js
@@ -7,4 +7,17 @@ export default function() {
       counter: 0
     }
   })
+
+  const vm2 = new Vue({
+    el: '#app-method',
+    data: {
+      name: 'Yoichi Kobayashi'
+    },
+    methods: {
+      greet: function(event) {
+        console.log('Hello ' + this.name + '!')
+        console.log(event)
+      }
+    }
+  })
 };
